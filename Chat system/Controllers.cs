ed223dea37a42923d19f2b56a464e4911ab7a6eb
mitkooooooooooooooooooooooo <146818015+mitkooooooooooooooooooooooo@users.noreply.github.com>
2026-03-26 09:30:05 +0200

    Implement ChatController for message handling

--- Chat system/Controllers.cs
+++ Chat system/Controllers.cs
@@ -1,8 +1,34 @@
-﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using ChatApp.Models;
 
-public class Class1
+namespace ChatApp.Controllers
 {
-	public Class1()
-	{
-	}
+    public class ChatController : Controller
+    {
+        private static List<KeyValuePair<string, string>> messages = new();
+
+        public IActionResult Show()
+        {
+            var model = new ChatViewModel();
+            foreach (var msg in messages)
+            {
+                model.Messages.Add(new MessageViewModel 
+                { 
+                    Sender = msg.Key, 
+                    MessageText = msg.Value 
+                });
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Send(ChatViewModel model)
+        {
+            messages.Add(new KeyValuePair<string, string>(
+                model.CurrentMessage.Sender, 
+                model.CurrentMessage.MessageText));
+
+            return RedirectToAction("Show");
+        }
+    }
 }
